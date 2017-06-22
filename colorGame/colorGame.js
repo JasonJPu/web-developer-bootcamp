ae08a0cf43bc1fbb1c2e7f203cbd07df2c6205fd
JasonJPu <cub1@live.ca>
2017-06-22 06:59:30 -0400

    Color game start

---
+++ colorGame/colorGame.js
@@ -0,0 +1,43 @@
+var colors = [
+  "rgb(255, 0, 0)",
+  "rgb(0, 150, 150)",
+  "rgb(255, 2550, 0)",
+  "rgb(55, 155, 0)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  // add initial colors to squares
+  squares[i].style.backgroundColor = colors[i];
+
+  // add click listeners to squares
+  squares[i].addEventListener("click", () => {
+    var clickedColor = squares[i].style.backgroundColor;
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
+    } else {
+      squares[i].style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
+    }
+  });
+}
+
+function changeColors(color) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
