a35d75e6d42c0767cb13b31b382d2a78e3386377
JasonJPu <cub1@live.ca>
2017-06-21 21:00:11 -0400

    Finished scorekeeper

--- JS/scoreKeeper/scoreKeeper.js
+++ JS/scoreKeeper/scoreKeeper.js
@@ -1,8 +1,12 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
 
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+
+var numInput = document.querySelector("input");
+var p = document.querySelector("p span");
 
 var p1Score = 0;
 var p2Score = 0;
@@ -12,8 +16,9 @@ var winningScore = 5;
 p1Button.addEventListener("click", () => {
   if (!gameOver) {
     p1Score++;
-    p1Display.textContent(p1Score);
+    p1Display.textContent = p1Score;
     if (p1Score === winningScore) {
+      p1Display.classList.add("winner");
       gameOver = true;
     }
   }
@@ -22,13 +27,30 @@ p1Button.addEventListener("click", () => {
 p2Button.addEventListener("click", () => {
   if (!gameOver) {
     p2Score++;
-    p2Display.textContent(p2Score);
+    p2Display.textContent = p2Score;
     if (p2Score === winningScore) {
+      p2Display.classList.add("winner");
       gameOver = true;
     }
   }
 });
 
-var reset = document.querySelector("click", () => {
+function reset() {
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+}
 
+resetButton.addEventListener("click", () => {
+  reset();
+});
+
+numInput.addEventListener("change", () => {
+  winningScore = Number(this.value);
+  p.textContent = winningScore;
+  reset();
 });
