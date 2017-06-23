7cec741a63717ed301b8b2b1cfbefa78d8e1005c
JasonJPu <cub1@live.ca>
2017-06-23 18:08:53 -0400

    Todo list functionality

---
+++ TodoList/assets/js/todos.js
@@ -0,0 +1,21 @@
+// Check off specific todos by clicking
+$("ul").on("click", "li", (event) => {
+  $(event.currentTarget).toggleClass("completed");
+});
+
+// Click on X to delete todo
+$("ul").on("click", "span", (event) => {
+  event.stopPropagation();
+  $(event.currentTarget).parent().fadeOut(500, () => {
+    $event.currentTarget.remove();
+  });
+});
+
+// Add new todo
+$("input[type='text']").on("keypress", (event) => {
+  if (event.which === 13) {
+    let input = $(event.currentTarget).val();
+    $(event.currentTarget).val("");
+    $("ul").append("<li><span>X</span> " + input + "</li>");
+  }
+});
