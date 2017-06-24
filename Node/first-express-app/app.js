4715a5e87169b005f8dcce4ac9ac59e7a96e2ed1
JasonJPu <cub1@live.ca>
2017-06-24 09:28:17 -0400

    Routing

--- Node/first-express-app/app.js
+++ Node/first-express-app/app.js
@@ -16,6 +16,20 @@ app.get("/dog", (req, res) => {
   res.send("MEOW!");
 });
 
+app.get("/r/:subredditName", (req, res) => {
+  var subreddit = req.params.subredditName;
+  res.send("Welcome to the " + subreddit + " subreddit!");
+})
+
+app.get("/r/:subredditName/comments/:id/:title/", (req, res) => {
+  res.send("Welcome to the comments page!");
+})
+
+// Catch all, but order of routes matters
+app.get("*", (req, res) => {
+  res.send("You are a star!");
+});
+
 // Tell Express to listen to requests (start server)
 app.listen(3000, () => {
   console.log("Started server on port 3000");
