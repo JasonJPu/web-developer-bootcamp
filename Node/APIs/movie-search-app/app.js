43739b74348d0becd80239ca0145a86bfefb1235
JasonJPu <cub1@live.ca>
2017-06-25 20:32:57 -0400

    Completed movie search API

--- Node/APIs/movie-search-app/app.js
+++ Node/APIs/movie-search-app/app.js
@@ -3,13 +3,21 @@ const request = require("request");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/results", (req, res) => {
-  request("http://www.omdbapi.com/?apikey=thewdb&s=california", (error, response, body) => {
+  const query = req.query.search;
+  const url = `http://www.omdbapi.com/?apikey=thewdb&s=${query}`;
+  request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-      const results = JSON.parse(body);
-      res.send(results["Search"][0]["Title"]);
+      const data = JSON.parse(body);
+      res.render("results", { data });
     }
   });
+});
+
+app.get("/", (req, res) => {
+  res.render("search");
 });
 
 app.listen(3000, () => {
