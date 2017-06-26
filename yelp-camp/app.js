b61ed3dd4ea2b0c6addf776afc4ba0c98880e174
JasonJPu <cub1@live.ca>
2017-06-26 06:11:25 -0400

    Added functionality of creating new campgroun

--- yelp-camp/app.js
+++ yelp-camp/app.js
@@ -1,30 +1,44 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+var campgrounds = [
+  {
+    name: "Salmon Creek",
+    image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+  },
+  {
+    name: "Granite Hill",
+    image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg",
+  },
+  {
+    name: "Mountain Goat's Rest",
+    image: "https://farm7.staticflickr.com/6186/6090714876_44d269ed7e.jpg",
+  },
+];
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-  var campgrounds = [
-    {
-      name: "Salmon Creek",
-      image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
-    },
-    {
-      name: "Granite Hill",
-      image: "https://farm5.staticflickr.com/4016/4369518024_0f64300987.jpg",
-    },
-    {
-      name: "Mountain Goat's Rest",
-      image: "https://farm7.staticflickr.com/6186/6090714876_44d269ed7e.jpg",
-    },
-  ];
-
   res.render("campgrounds", { campgrounds });
+});
+
+app.post("/campgrounds", (req, res) => {
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name, image };
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", (req, res) => {
+  res.render("new");
 });
 
 app.listen(3000, () => {
