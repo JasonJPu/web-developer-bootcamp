44dc02d0b0df6a255b69db5802a829db92c5fa6b
JasonJPu <cub1@live.ca>
2017-06-28 20:02:31 -0400

    Added index route and semantic UI

--- RESTful Routing/blog-app/app.js
+++ RESTful Routing/blog-app/app.js
@@ -1,14 +1,39 @@
-var express = require("express"),
-  bodyParser = require("body-parser"),
-  express = require("express"),
-  app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+const app = express();
+
+// APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var blogSchema = 
+//MONGOOSE MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now },
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTES
+app.get("/", (req, res) => {
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { blogs });
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log("Server is running!");
