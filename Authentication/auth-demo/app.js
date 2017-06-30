913f51b91d88137196da41b3de8541f9e5fce53f
JasonJPu <cub1@live.ca>
2017-06-30 15:49:42 -0400

    Added sign up to authentication

--- Authentication/auth-demo/app.js
+++ Authentication/auth-demo/app.js
@@ -11,6 +11,7 @@ mongoose.connect("mongodb://localhost/auth_demo");
 
 const app = express();
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(expressSession({
   secret: "This is a secret",
@@ -24,12 +25,42 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// Routes
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 app.get("/secret", (req, res) => {
   res.render("secret");
+});
+
+// Auth Routes
+
+// show sign up form
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+// handling user sign up
+app.post("/register", (req, res) => {
+  req.body.username
+  req.body.password
+  User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, () => {
+      res.redirect("/secret");
+    });
+  });
+});
+
+// Login Routes
+// render login form
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
 app.listen(3000, () => {
